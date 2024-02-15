cae1e4b6d3f8ca96681026ae4f11f2a4c2cb3410
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2024-02-15 19:07:06 -0300

    Add prefabs

--- WhatsBehindTheDoor/Assets/Scripts/GameManager.cs
+++ WhatsBehindTheDoor/Assets/Scripts/GameManager.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] List<GameObject> doorsToUse;
-    
+    [SerializeField] GameObject startDoor;
+    private readonly Vector3 doorSpawnPosition = new Vector3(-1.5f, 1.31f, 0);
+
     public void LoadNextDoor()
     {
+
+    }
+
+    public void Start()
+    {
+        Instantiate(startDoor, 
+                    doorSpawnPosition,
+                    Quaternion.identity
+                    );
 
     }
 }
