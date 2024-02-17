b9343c3adefd5ec48be63bfbc5695180256ae69e
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2024-02-17 19:07:36 -0300

    elevator door

---
+++ WhatsBehindTheDoor/Assets/Scripts/Puzzles/ElevatorDoor.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorDoor : MonoBehaviour
+{
+    [SerializeField] Door elevatorDoor;
+
+    [Header("UI")]
+    [SerializeField] GameObject elevatorButtonUp;
+    [SerializeField] GameObject elevatorButtonDown;
+
+    [Header("Switch")]
+    [SerializeField] Sprite elevatorSwitch;
+    [SerializeField] Sprite elevatorSwitchUp;
+    [SerializeField] Sprite elevatorSwitchDown;
+    private SpriteRenderer spriteRenderer;
+
+    [Header("Audio")]
+    [SerializeField] AudioClip elevatorMusic;
+    [SerializeField] AudioClip elevatorArrivedClip;
+    [SerializeField] float audioClipLengthWaitime;
+    [SerializeField] AudioSource mainAudioSource;
+    private AudioPlayer mainAudioPlayer;
+    private AudioSource elevatorAudioPlayer;
+
+    void Awake()
+    {
+        this.mainAudioPlayer = FindObjectOfType<AudioPlayer>();
+        this.elevatorAudioPlayer = GetComponent<AudioSource>();
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void CallElevator(bool up)
+    {
+        if(up)
+        {
+            this.spriteRenderer.sprite = this.elevatorSwitchUp;
+        }
+        else
+        {
+            this.spriteRenderer.sprite = this.elevatorSwitchDown;
+        }
+
+        this.elevatorButtonUp.SetActive(false);
+        this.elevatorButtonDown.SetActive(false);
+
+        StartCoroutine(ElevatorMoving());
+    }
+
+    IEnumerator ElevatorMoving()
+    {
+        this.mainAudioSource.Pause();
+        this.elevatorAudioPlayer.Play();
+
+        yield return new WaitForSeconds(audioClipLengthWaitime);
+        
+        this.mainAudioPlayer.PlayClip(this.elevatorArrivedClip, 1f);
+
+        this.elevatorAudioPlayer.Stop(); 
+        this.spriteRenderer.sprite = this.elevatorSwitch;
+        this.mainAudioSource.UnPause();
+        this.elevatorDoor.SetLocked(false);
+    }
+}
