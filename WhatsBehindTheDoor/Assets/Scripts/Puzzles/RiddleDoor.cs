a0560abb60cb49fbd09020e55176a26791c7d9e4
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2024-02-17 01:40:53 -0300

    add behavior to riddle door

--- WhatsBehindTheDoor/Assets/Scripts/Puzzles/RiddleDoor.cs
+++ WhatsBehindTheDoor/Assets/Scripts/Puzzles/RiddleDoor.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RiddleDoor : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
     [SerializeField] Sprite openViewframeSprite;
+
+    [Header("Canvas")]
     [SerializeField] GameObject openViewFrameButton;
     [SerializeField] GameObject riddleCanvas;
+
+    [Header("Text")]
+    [SerializeField] TextMeshProUGUI guardResponseText;
 
     void Awake()
     {
@@ -19,5 +25,26 @@ public class RiddleDoor : MonoBehaviour
         this.spriteRenderer.sprite = openViewframeSprite;
         this.openViewFrameButton.SetActive(false);
         this.riddleCanvas.SetActive(true);
+    }
+
+    public void CheckAnswer(string s)
+    {
+        string answer = s;
+        if(answer != "please")
+        {
+            StartCoroutine(GuardAnswer("Wrong..."));
+        } 
+        else
+        {
+            StartCoroutine(GuardAnswer("Very well, you may enter..."));
+            //unlock door
+        }
+    }
+
+    IEnumerator GuardAnswer(string guardAnswer)
+    {
+        this.guardResponseText.text = guardAnswer;
+        yield return new WaitForSeconds(4);
+        this.guardResponseText.text = "";
     }
 }
